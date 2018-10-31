50b3214d9312622483aae606f43ed6d6d3e0e307
Zhihao Li <515646122@qq.com>
2018-10-30 23:46:35 -0400

    unit test added

---
+++ server/ocr_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from ocr.ocr import ocr
+import json
+
+class TestOCR(unittest.TestCase):
+
+    def test_total(self):
+        result = ocr('upload/receipt.jpg')
+        result = json.loads(result)
+        self.assertLessEqual(result['accumTotal'], result['detectedTotal'])
+
+if __name__ == '__main__':
+    unittest.main()
