953d690b74c308a33721786c7f90628b61d10220
chelseazheng <chelseazheng826@gmail.com>
2018-11-24 12:32:19 -0500

    Added an animated footer

--- do_dutch/components/home_screen.js
+++ do_dutch/components/home_screen.js
@@ -2,22 +2,71 @@ import React, { Component } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Icon, Button, Container, Header, Content, Left } from "native-base";
 
+import BottomNavigation, {
+  FullTab
+} from 'react-native-material-bottom-navigation'
+
 export default class HomeScreen extends Component {
+
+  tabs = [
+    {
+      key: 'games',
+      // icon: 'gamepad-variant',
+      label: 'Games',
+      barColor: '#388E3C',
+      pressColor: 'rgba(255, 255, 255, 0.16)'
+    },
+    {
+      key: 'movies-tv',
+      // icon: 'movie',
+      label: 'Movies & TV',
+      barColor: '#B71C1C',
+      pressColor: 'rgba(255, 255, 255, 0.16)'
+    },
+    {
+      key: 'music',
+      // icon: 'music-note',
+      label: 'Music',
+      barColor: '#E64A19',
+      pressColor: 'rgba(255, 255, 255, 0.16)'
+    }
+  ]
+
+  renderIcon = icon => ({ isActive }) => (
+    <Icon size={24} color="white" name={icon} />
+  )
+
+  renderTab = ({ tab, isActive }) => (
+    <FullTab
+      isActive={isActive}
+      key={tab.key}
+      label={tab.label}
+      renderIcon={this.renderIcon(tab.icon)}
+    />
+  )
+
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => this.props.navigation.navigate("Camera")}
-        >
-          <Text style={styles.text}>Create a New Receipt</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => this.props.navigation.navigate("Form")}
-        >
-          <Text style={styles.text}>Check Form</Text>
-        </TouchableOpacity>
+        <View style={{ flex: 1 }}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate("Camera")}
+          >
+            <Text style={styles.text}>Create a New Receipt</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate("Form")}
+          >
+            <Text style={styles.text}>Check Form</Text>
+          </TouchableOpacity>
+        </View>
+        <BottomNavigation
+          onTabPress={newTab => this.setState({ activeTab: newTab.key })}
+          renderTab={this.renderTab}
+          tabs={this.tabs}
+        />
       </View>
     );
   }
@@ -26,9 +75,9 @@ export default class HomeScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#ffffff"
+    // justifyContent: "center",
+    // alignItems: "center",
+    // backgroundColor: "#ffffff"
   },
   button: {
     width: 250,
@@ -45,7 +94,7 @@ const styles = StyleSheet.create({
   },
   view: {
     flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center"
+    // justifyContent: "flex-end",
+    // alignItems: "center"
   }
 });
