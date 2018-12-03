acfb266e592ccf79541ba745d34f9c8ee5698ea9
chelseazheng <chelseazheng826@gmail.com>
2018-12-03 02:27:04 -0500

    New report

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -4,7 +4,7 @@ coverage run tests.py
 coverage xml --omit=/usr/* -o ../../tests/test-reports/coverage/coverage.xml
 
 # run react test
-cd ../../do_dutch/
-npm install
-react-native link
-npm run test
+#cd ../../do_dutch/
+#npm install
+#react-native link
+#npm run test
