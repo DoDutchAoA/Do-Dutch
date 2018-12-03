4b57bff2bf0afc5115a6efeaa7447ac1b6234d69
chelseazheng <chelseazheng826@gmail.com>
2018-12-03 01:32:57 -0500

    Managed files

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,3 +1,3 @@
 cd ../database/server
 coverage run tests.py
-coverage xml --omit=/usr/* -o ../../tests/test-reports/coverage.xml
+coverage xml --omit=/usr/* -o ../../tests/test-reports/coverage/coverage.xml
