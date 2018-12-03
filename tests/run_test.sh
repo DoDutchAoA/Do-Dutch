624ec6fa9cc53565a8caa8ad8f64b3cbc72825dd
chelseazheng <chelseazheng826@gmail.com>
2018-12-03 02:10:23 -0500

    Modified test script

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,3 +1,10 @@
+# run db test
 cd ../database/server
 coverage run tests.py
 coverage xml --omit=/usr/* -o ../../tests/test-reports/coverage/coverage.xml
+
+# run react test
+cd ../../do_dutch/
+npm install
+react-native link
+npm run test
